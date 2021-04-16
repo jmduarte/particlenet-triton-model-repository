334c7576a390b8692ee7f47b4790a1862a151150
Javier M. Duarte <jduarte@ucsd.edu>
2021-04-15 21:10:27 -0700

    first commit

---
+++ fetch_models.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ONNX particlenet
+mkdir -p model_repository/particlenet_onnx/1
+wget -O model_repository/particlenet_onnx/1/model.onnx \
+     https://github.com/cms-data/RecoBTag-Combined/raw/master/ParticleNetAK8/General/V01/particle-net.onnx
